b293785b6b3275d3ca470172acc76a615b75da91
unknown <connorgreenland05@gmail.com>
2022-12-12 19:53:28 -0700

    first push

---
+++ connorsPage.ts
@@ -0,0 +1,9 @@
+import { BasePage } from "./basePage";
+import { By } from "selenium-webdriver";
+
+export class connor extends BasePage {
+    constructor () {
+        super({url: })
+    }
+    
+}
